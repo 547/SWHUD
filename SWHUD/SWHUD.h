34e611e86f9463395007f60caa4ad38c63d7a351
Seven Wang <seven.wang@supernovachina.com>
2019-06-19 14:57:22 +0800

    Initial Commit

---
+++ SWHUD/SWHUD.h
@@ -0,0 +1,19 @@
+//
+//  SWHUD.h
+//  SWHUD
+//
+//  Created by Supernova SanDick SSD on 2019/6/19.
+//  Copyright © 2019 Seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWHUD.
+FOUNDATION_EXPORT double SWHUDVersionNumber;
+
+//! Project version string for SWHUD.
+FOUNDATION_EXPORT const unsigned char SWHUDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWHUD/PublicHeader.h>
+
+
